b595015da0294648ee40fb5a8f23382d426c22b6
HynekEducanet <hynek.sasinka@educanet.cz>
2022-09-19 09:43:45 +0200

    Dva Čtverce

---
+++ dva ctverce.py
@@ -0,0 +1,21 @@
+import turtle
+
+def ctverec():
+    for i in range(4):
+        turtle.forward(100)
+        turtle.left(90)
+
+ctverec()
+
+turtle.penup()
+turtle.backward(300)
+turtle.pendown()
+
+def druhyctverec():
+    for i in range (4):
+        turtle.forward(100)
+        turtle.left(90)
+
+
+druhyctverec()
+input("")
