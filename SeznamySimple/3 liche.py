3d10d6bffefaea8757b8c2f2057194fbd877e98d
HynekEducanet <hynek.sasinka@educanet.cz>
2022-10-14 09:07:33 +0200

    Úkol 3 upraven jako list

--- SeznamySimple/3 liche.py
+++ SeznamySimple/3 liche.py
@@ -4,6 +4,9 @@ if int(a % 2 == 1):
   print("Toto je liché číslo")
   for num in range(a, a + 16):
     if num % 2 != 0:
-      ele = print(num)
+      print(num)
+      list.append(num)
 elif int(a % 2 == 0):
   print("Toto je sudé číslo!")
+
+print(list)
